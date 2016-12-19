7a5bfcd9abeffacd04ddafccff042bc8e1884294
spudboy555 <kieranw6@hotmail.com>
2016-12-19 14:55:10 -0800

    Initial commit.

---
+++ 3D_Viewer/3D_Viewer/src/main.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+filename    main.c
+author      Kieran Williams
+DP email    kieran.williams@digipen.edu
+
+Copyright © 2016 DigiPen (USA) Corporation
+
+Brief Description:
+This file contains the entry into the program.
+
+******************************************************************************/
+
+// ---------------------------------------------------------------------------
+// Includes
+
+#include "AEEngine.h"
+#include <stdio.h>
+
+// ---------------------------------------------------------------------------
+// Functions
+
+// It's main. It launches everything.
+int main(void)
+{
+	//TODO: Load a .obj file.
+
+	//TODO: Open a window.
+}
